0d36c226420e6f725f7fe390668fcc5c2314363c
owden godson <owdeng@gmail.com>
2014-03-21 10:50:24 +0300

    added Appointments migration

---
+++ app/database/migrations/2014_03_20_224310_create_Appointments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('appointments',function($table){
+			$table->increments('id');
+			$table->integer('patient_id')->references('id')->on('patients');
+            $table->integer('doctor_id')->references('id')->on('users');
+            $table->String('date');
+			$table->timestamps();
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('appointments');
+	}
+
+}
