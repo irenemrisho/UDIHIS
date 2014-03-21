6de42bdd619e9f7c308a43f302b1611048ed1ce9
owden godson <owdeng@gmail.com>
2014-03-21 10:43:39 +0300

    added Users migration

---
+++ app/database/migrations/2014_03_20_223811_create_Users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users',function($table){
+			$table->increments('id');
+			$table->String('email');
+            $table->String('password');
+            $table->integer('level');
+            $table->String('first_name');
+            $table->String('middle_name');
+            $table->String('last_name');	
+            $table->String('address');
+            $table->String('designation');
+           	$table->String('contact'); 
+			$table->timestamps();
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
