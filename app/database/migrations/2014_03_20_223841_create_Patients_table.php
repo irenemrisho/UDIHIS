4a1088b106956fa743c1e18dadf1e9eb1bfaa165
owden godson <owdeng@gmail.com>
2014-03-21 10:44:57 +0300

    added Patients migration

---
+++ app/database/migrations/2014_03_20_223841_create_Patients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('patients',function($table){
+			$table->increments('id');
+			$table->String('first_name');
+			$table->String('middle_name');
+            $table->String('last_name');
+            $table->String('gender');
+            $table->date('birth_date');
+            $table->String('address');
+            $table->String('contact');	
+                        
+                       
+			$table->timestamps();
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('patients');
+	}
+
+}
