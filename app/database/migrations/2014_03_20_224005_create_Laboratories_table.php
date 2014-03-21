4efc1db24baf8a0e403067569061ed29f1d240a9
owden godson <owdeng@gmail.com>
2014-03-21 10:46:16 +0300

    added Laboratories migration

---
+++ app/database/migrations/2014_03_20_224005_create_Laboratories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLaboratoriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('laboratories',function($table){
+			$table->increments('id');
+			$table->integer('pv_id')->references('id')->on('patients_visits');
+			$table->String('test_type');
+	        $table->String('result');
+	        $table->date('date');
+	        $table->integer('service_id')->references('id')->on('services');
+			$table->timestamps();
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('laboratories');
+	}
+
+}
