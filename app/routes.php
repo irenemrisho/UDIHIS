1f303ae835cf917f447b8ee2bef2e74c699723a8
owden godson <owdeng@gmail.com>
2014-04-04 14:46:59 +0300

    added routes

--- app/routes.php
+++ app/routes.php
@@ -12,17 +12,24 @@
  */
 
 //Route::controller('/', 'UserController');
-
 Route::get('/', 'UserController@getIndex');
 Route::get('login', 'UserController@getIndex');
+Route::post('/login', 'UserController@login');
 Route::get('pharmacy', 'Pharmacy@getIndex');
 Route::get('admin','Admin@getIndex');
-Route::get('/addUser','UserController@addUser');
 Route::post('/users/store','UserController@storeUser');
-Route::post('/login', 'UserController@login');
 Route::get('/manage_user','Admin@manage_user');
 Route::post('/manage_user','Admin@addUser');
 Route::get('/logout','UserController@logout');
+Route::get('forgot_password','UserController@forgotPassword');
+//Doctor routes
+Route::get('doctor','DoctorController@index');
+Route::get('appointment','DoctorController@appointment');
+Route::get('patients','DoctorController@patients');
+Route::get('profile','DoctorController@profile');
+Route::get('reports','DoctorController@reports');
+Route::get('prescription','DoctorController@prescription');
+
 
 
 //Route::get('/login', function() {
