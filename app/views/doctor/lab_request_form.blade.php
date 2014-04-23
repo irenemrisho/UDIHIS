5d9fd88dd078a2dafb5e35896f1672b794d913f0
sunducharles <sunducharles@gmail.com>
2014-04-23 04:00:21 -0700

    added lab request form

---
+++ app/views/doctor/lab_request_form.blade.php
@@ -0,0 +1,253 @@
+@extends('dashboard')
+@section('main')
+<div class="widget-header">
+                        <i class="icon-th-list"></i>
+                        <h3>LABORATORY REQUEST/REPORT FORM</h3>
+</div> <!-- /widget-header -->
+<div id="" class="widget-content">
+<div class="row">
+<form class="">
+
+<div class="span2 control-group">
+    <h5>1.HAEMATOLOGY</h5>
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1"> Bs for MPs.../200WBC
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> Haemoglobin.../dl
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Sickling Test...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Haemogram(Full Blood Picture)...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> ESR...mm/hr
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Bleeding Time...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Clotting Time...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> P.T
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> PTT
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> B/Group RH Factors...
+    </label>
+    <h5>2.SEROLOGY</h5>
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1"> HIV...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> CD4/CD8 Profile...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> VDRL...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Widal Test...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> TPHA...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> HB sAg...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> R.Factors...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> H.PYLORI...
+    </label>
+</div>
+
+<div class="span2 control-group">
+	<h5>3.URINALYSIS</h5>
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1"> Color...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> PH...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> SP Gravity...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Glucose...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Protein...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Ketones...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Urobilinogen...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> WBs...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> RBC...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> EP Sales...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Yeast Cells...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Crystals...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Ova...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Others...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> C/S...
+    </label>
+    <h5>4.OTHERS</h5>
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1"> HVs
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> Semenalanalysis
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> PSA
+    </label>
+</div>
+
+<div class="span2 control-group">
+<h5>5.BIOCHEMISTRY</h5>
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1">  RBG.Fasting...mmo/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> Random...mml/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Cholesterol...mml/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Uric Acid...mmol/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> S.Creatinine...umol
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Urea...mmol/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Bilirubin Total...umo/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Direct...umol/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Indirect...umol/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> ALAT...lu/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> ASAT...lu/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Alkaline phos...lu/l
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> S.Amylase...
+    </label>
+
+    <h5>6.ELECTROLYTE</h5>
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1"> K+
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> Na
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Cl
+    </label>
+    <div class="control-group">
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1"> HCO3
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> Ca
+    </label>
+</div>
+</div>
+
+<div class="span2 control-group">
+<h5>7.STOOL</h5>
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1"> Appearance..
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> WBC...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> RBC...
+    </label>
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1"> Macrophages..
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> Ova/Cyst...
+    </label>
+    
+    <h5>8.BODY FLUIDS(S'fy)</h5>
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1"> Appearance...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> ZN-Stain...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Gram Stain...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> C/S...
+    </label>
+
+    <h5>9.HORMONES</h5>
+    <label class="checkbox">
+    <input type="checkbox" id="inlineCheckbox1" value="option1"> Prolactin...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox2" value="option2"> FSH...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> L.H...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> TSH,T3,T4...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Progesterone...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Testosterone...
+    </label>
+    <label class="checkbox ">
+    <input type="checkbox" id="inlineCheckbox3" value="option3"> Androgen...
+    </label>
+</div>
+<div class="form-controls">
+        <button class="btn btn-primary">Lab Test</button>
+        
+</div>
+</form>
+</div>
+</div>
+@stop
