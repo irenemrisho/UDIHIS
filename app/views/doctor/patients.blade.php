2d429e317d439cc07541273545c06fbe0600c0aa
owgee <owdeng@gmail.com>
2014-05-07 11:50:30 +0300

    added to the doctor

--- app/views/doctor/patients.blade.php
+++ app/views/doctor/patients.blade.php
@@ -88,6 +88,21 @@
 					
 				
 			</div> <!-- /span9 -->
+			<script type="text/javascript">
+			$(document).ready(function(){
+					var elem = '<div class="well"><a href="google.com">Message one, From someone.</a></div>'+
+							    '<div class="well"><a href="google.com">Message one, From someone.</a></div>'+
+							    '<button id="close-popover" data-toggle="clickover" class="btn btn-small btn-primary pull-right" onclick="$(&quot;#meddelanden&quot;).popover(&quot;hide&quot;);">Close please!</button>';
+
+			$('#nextVisit').click(function(elem){
+				$('#meddelanden').popover({animation:true, content:elem, html:true});
+			});				    
+
+			
+			});
+
+			</script>
+			
 			
 		@stop
 
