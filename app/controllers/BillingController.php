bf7a95aee04b0fc9a3517e8b9873c8b40cac6089
irenemrisho <irenemrisho@gmail.com>
2014-05-21 21:48:15 +0300

    controller billing added

--- app/controllers/BillingController.php
+++ app/controllers/BillingController.php
@@ -26,9 +26,72 @@ class BillingController  extends BaseController{
     }
     
     public function getInsurance(){
-            return View::make("billing.Insurance_management");
+
+             $campanies = InsuranceCompany::orderBy('name')->get();
+
+            return View::make("billing.Insurance_management")->with('campanies',$campanies);
+    }
+
+    public function addInsurance(){
+
+        $campany = new InsuranceCompany;
+
+            $campany->name = Input::get('campany_name');
+            $campany->contact_name = Input::get('contact_person');
+            $campany->pobox = Input::get('P_O_BOX');
+            $campany->city = Input::get('City');
+            $campany->contact_email = Input::get('email');
+            $campany->payement_type= Input::get('payment_type');
+            $campany->from = Input::get('contract_from');
+            $campany->to = Input::get('contract_to');
+            
+            $campany->save();
+
+
+            if ($campany) {
+                $campany_id = $campany->id;      
+
+               return Redirect::to('add_campany_price?msg=1&cmp='.$campany_id);
+
+                        }else{
+                            
+                            return Redirect::to('Insurance_management?msg=2');
+                        }
+    
     }
     
+     public function getEditCampany()
+    {
+        # code...
+        return View::make("billing.edit_insurance_campany");
+    }
+
+    public function UpdateCampany()
+    {
+
+        $inputs = Input::all();   
+        $service = InsuranceCompany::where('id',$_GET['edit'])
+                                ->update(array(
+                "name"=>$inputs['campany_name'],
+                "contact_name"=>$inputs['contact_person'],
+                "pobox"=>$inputs['P_O_BOX'],
+                "contact_email"=>$inputs['email'],                
+                "city"=>$inputs['City'],
+                "payement_type"=>$inputs['payment_type'],
+                "from"=>$inputs['contract_from'],
+                "to"=>$inputs['contract_to'],
+
+                ));
+            if ($service) {
+                            
+               return Redirect::to('Insurance_management?msg=3');
+                        }else{
+                            
+                            return Redirect::to('Insurance_management?msg=4');
+                        }
+
+        # code...
+    }
 
     public function getService(){
      
@@ -37,7 +100,7 @@ class BillingController  extends BaseController{
 			$service = Service::where('id',$_GET['edit'])
                                 ->update(array(
 				"name"=>$inputs['name'],
-				"price"=>$inputs['Price']
+				"price"=>$inputs['price']
 
 				));
 			if ($service) {
@@ -63,12 +126,11 @@ class BillingController  extends BaseController{
     public function addService(){
                 
                  
-               	$inputs = Input::all();
+            $inputs = Input::all();
 			$service = Service::create(array(
 				"name"=>$inputs['service_name'],
 				"price"=>$inputs['service_price']
 				
-
 				));
 			if ($service) {
                             
@@ -83,6 +145,10 @@ class BillingController  extends BaseController{
             return View::make("billing.reports_billing");
     }
     
+    public function get_patient_invoice(){
+            return View::make("billing.invoice_pdf");
+    }
+
     public function getProfile(){
             return View::make("billing.profile_billing");
     }
@@ -124,8 +190,45 @@ class BillingController  extends BaseController{
    
     }
 
+  public function destroyCampany($id){
+            $campany = InsuranceCompany::find($id);
+            $campany->delete();
+        }
+ 
 
+  public function addCampanyPrice(){
+
+        return View::make("billing.add_campany_price");
+  }
+
+  public function saveCampanyPrice(){
+
+        $Services =Service::all(); 
+        $campany_id = $_GET['cmp'];                          
+            
+        foreach ($Services as $Service) {
+         
+
+            $price_companie = Price_company::create(array(
+
+                "service_id"=>$Service->id,
+                "company_id"=>$campany_id,
+                "price"=>Input::get('service'.$Service->id.'')
+
+                ));
+
+        }   
+
+        if ($price_companie) {
+                
+
+               return Redirect::to('Insurance_management?msg=1');
+
+                        }else{
+                            
+                            return Redirect::to('Insurance_management?msg=2');
+                        }                                  
+  }
 
  }
- 
  
