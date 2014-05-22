da1769c722eabf4fbfa9ce340a9c996a019fbed4
owgee <owdeng@gmail.com>
2014-05-22 21:15:36 +0300

    added a dashboard highchart

--- app/views/doctor/doctor.blade.php
+++ app/views/doctor/doctor.blade.php
@@ -1,50 +1,4 @@
 @extends('dashboard')
-@section('page_specific_scripts-highchart')
-	<!-- Highchart includes -->
-	{{HTML::script('packages/charts/js/highcharts.js')}}
-	<script type="text/javascript">
-			$(function () {
-        $('#bar-chart').highcharts({
-            chart: {
-                type: 'line'
-            },
-            title: {
-                text: 'Weekly Patients Visits'
-            },
-            subtitle: {
-                text: 'Source: Doctor'
-            },
-            xAxis: {
-                categories: ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
-            },
-            yAxis: {
-                title: {
-                    text: 'Number (#)'
-                }
-            },
-            plotOptions: {
-                line: {
-                    dataLabels: {
-                        enabled: true
-                    },
-                    enableMouseTracking: false
-                }
-            },
-            series: [{
-                name: 'Male',
-                data: [12.0, 15.9, 19.5, 23.5, 28.4, 32.5, 38.2]
-            }, 	{
-                name: 'Female',
-                data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2]
-            },
-            	{
-                name: 'Children',
-                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0]
-            }]
-        });
-    });
-	</script>
-@stop
 @section('main')
 <h1 class="page-title">
 			<i class="icon-home"></i>
@@ -64,4 +18,67 @@
 				<div id="bar-chart" class="chart-holder"></div> <!-- /bar-chart -->				
 			</div> <!-- /widget-content -->
 		
+@stop
+@section('page_specific_scripts-highchart')
+	<!-- Highchart includes -->
+	{{HTML::script('packages/charts/js/highcharts.js')}}
+	{{HTML::script('packages/charts/js/modules/exporting.js')}}
+	<script type="text/javascript">	
+		
+$(function () {
+	
+        $('#bar-chart').highcharts({
+            title: {
+                text: 'Weekly Patients Visits',
+                x: -20 //center
+            },
+            subtitle: {
+                text: 'Source: Doctor',
+                x: -20
+            },
+            xAxis: {
+                categories: ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat',
+                    'Sun']
+            },
+            yAxis: {
+                title: {
+                    text: 'Number (#)'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                valueSuffix: ''
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            plotOptions: {
+            series: {
+                animation: {
+                    duration: 5000
+	                }
+	            }
+	        },
+	        
+            series: [{
+                name: 'Male',
+                data: [15, 6, 19, 14, 18, 21, 25]
+            }, {
+                name: 'Female',
+                data: [10, 12, 23, 8, 17.0, 22, 24]
+            }, {
+                name: 'Children',
+                data: [20, 16, 7, 8, 13, 17, 30]
+            }]
+        });
+    });
+
+	</script>
 @stop
