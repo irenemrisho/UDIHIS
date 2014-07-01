89f01b12c3075f5a3f7797d380710d1f78518a77
irenemrisho <irenemrisho@gmail.com>
2014-07-01 12:02:45 +0300

    supplier user added

--- app/views/hr/person_other_information.blade.php
+++ app/views/hr/person_other_information.blade.php
@@ -166,7 +166,7 @@
                         <div class="control-group">
                             <label class="control-label" for="designation">Designation</label>
                             <div class="controls">
-                                {{Form::select('level',array(''=>'','1'=>'Pharmacist','2'=>'Lab Technician','3'=>'Receptionist','4'=>'Doctor','5'=>'Accountant','6'=>'Nurse','7'=>'HR Officer'), '', array('required'=>'required'))}}
+                                {{Form::select('level',array(''=>'','1'=>'Pharmacist','2'=>'Lab Technician','3'=>'Receptionist','4'=>'Doctor','5'=>'Accountant','6'=>'Nurse','7'=>'HR Officer','8'=>'supplier'), '', array('required'=>'required'))}}
 
                             </div>
 
